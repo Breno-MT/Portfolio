8b08ac6ca9700dd07bd5b35a4aa6a21295fa4284
Breno-MT <hentgenp@gmail.com>
2024-02-02 03:21:37 -0300

    REFACT: added ShowMore component, added methods and data, changed buttons show style, changed a few texts as well

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,18 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-      <v-img height="300" src="@/assets/logo.svg" />
+      <v-img
+        height="300"
+        src="https://avatars.githubusercontent.com/u/95316873?v=4"
+      />
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+      <div class="text-body-2 font-weight-medium mb-n1">
+        Welcome to my Portfolio!
+      </div>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+      <h1 class="text-h2 font-weight-bold">I'm Breno Martins!</h1>
 
-      <div class="py-14" />
+      <div class="py-4" />
 
       <v-row class="d-flex align-center justify-center">
         <v-col cols="auto">
@@ -18,58 +23,74 @@
             target="_blank"
             variant="text"
           >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
+            <v-icon color="primary" icon="mdi-linkedin" size="large" start />
 
-            Components
+            LinkedIn
           </v-btn>
         </v-col>
 
         <v-col cols="auto">
           <v-btn
-            color="primary"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
+            href="https://github.com/Breno-MT"
             min-width="164"
             rel="noopener noreferrer"
             target="_blank"
             variant="text"
           >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
+            <v-icon icon="mdi-github" size="large" start />
 
-            Community
+            Github
           </v-btn>
         </v-col>
       </v-row>
+      <v-col cols="auto">
+        <v-btn
+          @click="showMoreAboutMe()"
+          color="primary"
+          min-width="228"
+          rel="noopener noreferrer"
+          size="x-large"
+          target="_blank"
+          variant="flat"
+        >
+          <v-icon
+            v-if="showMore"
+            icon="mdi-arrow-collapse-down"
+            size="large"
+            start
+          />
+          <v-icon
+            v-if="!showMore"
+            icon="mdi-arrow-collapse-up"
+            size="large"
+            start
+          />
+
+          Lorem, ipsum.
+        </v-btn>
+      </v-col>
+      <ShowMore :expand="showMore" />
     </v-responsive>
   </v-container>
 </template>
 
 <script setup>
-  //
+import ShowMore from "@/components/ShowMore.vue";
+import useAppStore from "@/store/app";
+
+// Data:
+const store = useAppStore();
+const showMore = ref(store.showMoreAboutMe);
+
+// Methods:
+const showMoreAboutMe = () => {
+  store.$patch((state) => {
+    state.valueShowMoreAboutMe = !showMore.value;
+  });
+  showMore.value = store.showMoreAboutMe;
+  console.log("changed: ", store.showMoreAboutMe);
+  console.log("changed not getters: ", showMore.value);
+};
+
+console.log("teste2: ", store.showMoreAboutMe);
 </script>
