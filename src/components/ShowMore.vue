36a81c2e7a3f13aaf065bcbda53d2ff425895e96
Breno-MT <hentgenp@gmail.com>
2024-02-02 03:20:58 -0300

    REFACT: created ShowMore component (W.I.P)

---
+++ src/components/ShowMore.vue
@@ -0,0 +1,16 @@
+<template>
+   <v-col class="shrink">
+      <v-expand-transition v-show="store.showMoreAboutMe">
+        <p class="text-h4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, et recusandae doloribus, nulla consectetur cumque quod quo perspiciatis similique cum rerum temporibus blanditiis inventore ipsa cupiditate labore nesciunt! Placeat alias asperiores assumenda, nemo sapiente rem. Est facere omnis minima doloribus corporis tempora deserunt placeat perferendis deleniti laborum, adipisci qui cupiditate!</p>
+      </v-expand-transition>
+    </v-col>
+</template>
+
+<script setup>
+import useAppStore from "@/store/app";
+
+// Data:
+const store = useAppStore();
+</script>
+
+<style scoped></style>
